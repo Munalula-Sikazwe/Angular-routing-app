44df486dbcb8deb7499ab3b2c1b824280f27e565
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-04 22:01:11 +0200

    Implemented the can activate child interface to protect children of the servers route.

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -13,7 +13,7 @@ const appRoutes: Routes = [
   {path: 'users', component: UsersComponent, children: [{path: ':id/:name', component: UserComponent}]},
 
   {
-    path: 'servers',canActivate:[AuthGuardService], component: ServersComponent,
+    path: 'servers',canActivateChild:[AuthGuardService], component: ServersComponent,
     children: [{path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent},
     ]
