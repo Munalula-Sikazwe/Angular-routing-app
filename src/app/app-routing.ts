fbb8fde5fa2ca604a7b6b14276c1c93050d73183
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-04 23:19:37 +0200

    Created the can deactivate guard service and applied it to the edit server page.

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -8,6 +8,7 @@ import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuardService} from "./auth-guard.service";
+import {CanDeactivateGuardService} from "./can-deactivate-guard.service";
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent, children: [{path: ':id/:name', component: UserComponent}]},
@@ -15,7 +16,7 @@ const appRoutes: Routes = [
   {
     path: 'servers',canActivateChild:[AuthGuardService], component: ServersComponent,
     children: [{path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent},
+      {path: ':id/edit', component: EditServerComponent,canDeactivate:[CanDeactivateGuardService]},
     ]
   },
   {path:'page-not-found',component:PageNotFoundComponent},
--- src/app/can-deactivate-guard.service.ts
+++ src/app/can-deactivate-guard.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
-export class CanDeactivateGuardService {
+export class CanDeactivateGuardService implements CanDeactivate<CanComponentDeactivate>{
+  canDeactivate(component: CanComponentDeactivate, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree { return component.canDeactivate()
+
+  }
 
   constructor() { }
+}
+export interface CanComponentDeactivate{
+  canDeactivate:()=> Observable<boolean>|Promise<boolean>|boolean;
 }
