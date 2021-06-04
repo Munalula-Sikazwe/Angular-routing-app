2b21a2b40878ec3e47395567ccb5163e2fb7daf4
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-04 09:46:29 +0200

    Outsourced the app routing to an external app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,11 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
-import {RouterModule, Routes} from "@angular/router";
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRouting} from "./app-routing";
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, children: [{path: ':id/:name', component: UserComponent}]},
 
-  {
-    path: 'servers', component: ServersComponent,
-    children: [{path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent},
-    ]
-  },
-  {path:'page-not-found',component:PageNotFoundComponent},
-  {path:'**',redirectTo:'/page-not-found'}
-]
 
 @NgModule({
   declarations: [
@@ -42,10 +31,11 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRouting,
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
 }
