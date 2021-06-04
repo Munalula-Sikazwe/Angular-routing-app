b7ca93975254d9811d00b57365e944694eec0edf
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-04 23:41:30 +0200

    implemented server canDeactivate interface and service on server route to guard from moving away from route successfully.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Observable} from "rxjs";
 import { ServersService } from '../servers.service';
 import {ActivatedRoute, Params} from "@angular/router";
 
@@ -13,6 +13,7 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 allowEdit = false;
+changesSaved = false;
   constructor(private serversService: ServersService, private route:ActivatedRoute) { }
 
   ngOnInit() {
@@ -29,5 +30,15 @@ allowEdit = false;
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
   }
-
+canDeactivate(): Observable<boolean>|Promise<boolean>|boolean{
+    if(!this.allowEdit){
+      return true;
+    }else{
+      if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved){
+        return confirm("Do you want to discard the changes")
+      }else{
+        return true;
+      }
+    }
+}
 }
