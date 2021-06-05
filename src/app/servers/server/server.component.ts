04fe6c52b6a44e57fbdb8dfe0079373f5b2118c1
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-05 02:13:58 +0200

    server now uses the resolver to get data dynamically without using the params directly.

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -14,11 +14,9 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService,private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-
-     this.route.params.subscribe((params:Params)=>{
-      this.server = this.serversService.getServer(+params['id']);
+   this.route.data.subscribe((data:Data)=>{
+     this.server = data['server'];
+   })
       }
     );
 
