e27bdc9165250fccf4be50141059c10963c44653
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-05 02:08:05 +0200

    mapped server observable to server route

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -10,13 +10,14 @@ import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuardService} from "./auth-guard.service";
 import {CanDeactivateGuardService} from "./can-deactivate-guard.service";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolverService} from "./servers/server/server-resolver.service";
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent, children: [{path: ':id/:name', component: UserComponent}]},
 
   {
     path: 'servers',canActivateChild:[AuthGuardService], component: ServersComponent,
-    children: [{path: ':id', component: ServerComponent},
+    children: [{path: ':id', component: ServerComponent,resolve:{server:ServerResolverService}},
       {path: ':id/edit', component: EditServerComponent,canDeactivate:[CanDeactivateGuardService]},
     ]
   },
