bb00fad4a85cb9b17840d50c965072a85be980bc
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-06 12:08:45 +0200

    Removed redundant params import.

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {Params, ActivatedRoute, Router, Data} from "@angular/router";
+import { ActivatedRoute, Router, Data} from "@angular/router";
 
 @Component({
   selector: 'app-server',
